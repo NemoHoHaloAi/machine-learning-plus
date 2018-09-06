c0c713213c36f76445149a7dd219245994a64de2
helong <nevermore.he@aispeech.com>
2018-09-06 11:30:26 +0800

    测试完成绘制学习量脚本，明显看到从8月中后开始发力的特点

--- gen_study_daily.py
+++ gen_study_daily.py
@@ -12,6 +12,7 @@ ps              Please be pythonic.
 
 import sys
 import os
+from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
@@ -27,8 +28,6 @@ def main():
     程序入口
     """
     enviroment_init()
-    #xfmt = mdates.DateFormatter('%y-%m-%d')
-    #ax.xaxis.set_major_formatter(xfmt)
 
     lines=[]
     with open('README.md', 'r') as fr:
@@ -37,6 +36,7 @@ def main():
         for line in fr.readlines():
             if '>>START<<' in line:
                 start = True
+                continue
             elif '>>END<<' in line:
                 end = True
             if end:
@@ -44,21 +44,30 @@ def main():
             if start:
                 lines.append(line)
 
-    print lines
-    times = [line.split(',')[0] for line in lines]
+    times = [line.split(',')[0].strip() for line in lines]
+    times = [datetime.strptime(d, '%Y/%m/%d').date() for d in times]
     print times
     # 监督学习-76,监督学习-95
     dailies = []
+    offset = 0
     for line in lines:
         _from,_to = line.split(',')[1:]
         _from,_from_v = _from.split('-')
         _to,_to_v = _to.split('-')
-        dailies.append(_to_v-_from_v if _to == _from else 100+_to_v-_from_v)
+        if _from != _to:
+            offset = offset + 100
+        _from_v = float(_from_v)
+        _to_v = float(_to_v)
+        dailies.append(_to_v + offset)
     print dailies
     plt.plot(times,dailies)
 
-    plt.ylabel(u'学习量',fontproperties='SimHei')
-    plt.xlabel(u'日期',fontproperties='SimHei')
+    ax = plt.gca()
+    xfmt = mdates.DateFormatter('%y-%m-%d')
+    ax.xaxis.set_major_formatter(xfmt)
+
+    plt.ylabel(u'study_daily',fontproperties='SimHei')
+    plt.xlabel(u'date',fontproperties='SimHei')
 
     plt.grid(True)
     plt.show()
